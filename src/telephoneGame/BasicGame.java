c2cec99469fb08587f260ae26db8ff90f97ff36d
zhangqiang <55468499@163.com>
2018-10-11 10:33:50 +0800

    测试时2

--- src/telephoneGame/BasicGame.java
+++ src/telephoneGame/BasicGame.java
@@ -14,7 +14,7 @@ public class BasicGame {
 
 	public static void main(String[] args) {
         players = GamePeopleListFactory.getPeopleList();
-        //游戏初始化输入第一个人,测试
+        //游戏初始化输入第一个人,测试1
         System.out.println("Please enter a sentence to start");
         Person firstPlayer = players.getHead().getData();
 		Scanner scanner = new Scanner(System.in);   //控制台获取第一个人说的话
